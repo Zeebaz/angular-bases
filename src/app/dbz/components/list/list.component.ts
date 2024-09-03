b0e9a7f3b177ea8b996258d59bbb7314ff38aa25
Zeebaz <jairosebastianrp@gmail.com>
2024-09-03 15:54:50 -0600

    Service implementation

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+})
+export class ListComponent {
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10,
+    },
+  ];
+
+  onDeleteCharacter(id: string): void {
+    this.onDelete.emit(id);
+  }
+}
